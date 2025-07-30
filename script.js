3eb8b36bd1ef872cf240f3b126f99396a5b43457
danillq-coder <svorobovic.daniel@gmail.com>
2025-07-30 23:22:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,15 @@ document.getElementById('changeColorBtn').addEventListener('click', function() {
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = randomColor;
 });
- //window.addEventListener("scroll", function () {
- // const info = document.getElementById("scrollInfo");
- // if (window.scrollY > 200) { // когда прокрутка > 200px
-  //  info.classList.add("show");
-//  } else {
- //   info.classList.remove("show");
- // }
-//});
-//const scrollInfo = document.getElementById('scrollInfo');
+
+const scrollInfo = document.getElementById('scrollInfo');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 200) {
-    // показываем полоску
     scrollInfo.classList.add('active');
     scrollInfo.classList.remove('hidden');
   } else {
-    // прячем полностью
     scrollInfo.classList.remove('active');
     scrollInfo.classList.add('hidden');
   }
 });
-
