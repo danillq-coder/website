68b14958ff57be5df1901b8ce40611d053157c73
danillq-coder <svorobovic.daniel@gmail.com>
2025-07-30 22:04:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// eto komanda stoby vyskakivala kak otkryvajes sajt alert("Привет! Я JS из отдельного файла");
-
 document.getElementById('changeColorBtn').addEventListener('click', function() {
-  // Меняем цвет фона body на случайный
   const colors = ['#fefbd8', '#6b3e26', '#80ced6', '#f18973', '#d5f4e6'];
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   document.body.style.backgroundColor = randomColor;
