e21a64c016078ff998faed0d5f89df854c76b13b
danillq-coder <svorobovic.daniel@gmail.com>
2025-07-30 22:08:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("Script loaded");
+
 document.getElementById('changeColorBtn').addEventListener('click', function() {
   const colors = ['#fefbd8', '#6b3e26', '#80ced6', '#f18973', '#d5f4e6'];
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
