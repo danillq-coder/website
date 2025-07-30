981ad1168b96ac3dd142cb9e7f70725b53711025
danillq-coder <svorobovic.daniel@gmail.com>
2025-07-30 21:57:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert("Привет! Я JS из отдельного файла");
+
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+  // Меняем цвет фона body на случайный
+  const colors = ['#fefbd8', '#6b3e26', '#80ced6', '#f18973', '#d5f4e6'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
+});
